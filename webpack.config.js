92d785fd78f91e7a4635f325a7f4d6fceaff6912
Taylor Phillips <taylorphillips133@gmail.com>
2020-06-22 14:13:05 -0700

    add the changes to template to make title to Simon and co authors Taylor and Spencer
    Co-Authore-By: Spencer Moody <spencer.moody@outlook.com>

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
     new UglifyJsPlugin({ sourceMap: true }),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'APPLICATION NAME',
+      title: 'Simon the Game',
       template: './src/index.html',
       inject: 'body'
     })
