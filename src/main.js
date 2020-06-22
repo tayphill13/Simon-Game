34edf69ea9399e34837feab2a7bd61a1694272e5
Taylor Phillips <taylorphillips133@gmail.com>
2020-06-22 16:16:56 -0700

    begin our front end main.js
    Co-Authored-By: Spencer Moody <spencer.moody@outlook.com>

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
 import './styles.css';
+import { Simon } from './simon';
+
+$(document).ready(function(){
+  let newGame = new Simon();
+    $(".start").submit(function(event){
+      event.preventDefault();
+
+    })
+})
--- src/simon.js
+++ src/simon.js
@@ -4,13 +4,20 @@ export function Simon()  {
   this.colors = ["red", "blue", "green", "yellow"];
   this.userPattern = [];
   this.aIPattern = [];
-  this.score = 0;
 }
 
 Simon.prototype.randomColor = function() {
   let randomPattern = Math.floor(Math.random()*4);
   this.aIPattern.push(this.colors[randomPattern]);
 };
-// Simon.prototype.arrayGo() {
-//   for (let i=0; i>= this.colors.length; i++)
-// }
+
+Simon.prototype.compareColors = function() {
+  if(this.aIPattern.length!=this.userPattern.length)
+    return "False";
+  else {
+    for(let i=0;i<this.aIPattern.length;i++) 
+      if(this.aIpattern[i]!=this.userPattern[i])
+        return "False";
+    return "True";
+  }
+};
